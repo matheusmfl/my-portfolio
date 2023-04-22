20300a9b38cadc0d0c0313e16817d3a477b3b6b7
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-22 11:37:50 -0300

    Create Hero Section, config lib Writter react

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,12 +5,14 @@ export default function Hero() {
   return (
     <div>
       <p>
-        Seila fulano{' '}
         <Typewriter
-          words={['olá', 'meu nome é ', 'Matheus']}
-          typeSpeed={80}
+          words={[
+            ' Olá, meu nome é Matheus Fonteles!',
+            ' Desenvolvedor de aplicações web ',
+            ' <Programador front-end/>',
+          ]}
+          delaySpeed={2000}
           loop={true}
-          cursorBlinking={true}
         />
       </p>
     </div>
