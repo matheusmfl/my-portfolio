1d885b5e6b3430de81b50683c7eebc34e8f30af2
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-22 14:08:50 -0300

    finalized first step on hero content, animate the background circles, set the motion curves, create buttons

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,20 +1,44 @@
 'use client'
-import { Typewriter } from 'react-simple-typewriter'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import { BackgroundCircles } from './BackgroundCircles'
+import Image from 'next/image'
 
 export default function Hero() {
+  const [text] = useTypewriter({
+    words: [
+      'Olá, meu nome é Matheus Fonteles!',
+      'Desenvolvedor de aplicações web ',
+      '<Programador front-end/>',
+    ],
+    loop: true,
+    delaySpeed: 1500,
+  })
   return (
-    <div>
-      <p>
-        <Typewriter
-          words={[
-            ' Olá, meu nome é Matheus Fonteles!',
-            ' Desenvolvedor de aplicações web ',
-            ' <Programador front-end/>',
-          ]}
-          delaySpeed={2000}
-          loop={true}
-        />
-      </p>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <Image
+        src={'https://avatars.githubusercontent.com/u/92957086?v=4'}
+        alt="avatar"
+        className="relative rounded-full mx-auto object-cover w"
+        width={128}
+        height={128}
+      />
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Desenvolvedor de software
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+
+        <div>
+          <button className="heroButton">Sobre mim</button>
+          <button className="heroButton">Experiência</button>
+          <button className="heroButton">Habilidades</button>
+          <button className="heroButton">Projetos</button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ import Hero from './components/Hero'
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll  z-0">
       <Header />
-      <section id="hero">
-        <Hero></Hero>
+      <section id="hero" className="snap-center">
+        <Hero />
       </section>
     </div>
   )
