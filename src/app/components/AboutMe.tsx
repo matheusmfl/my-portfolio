f8479461efabd5f6c8423803f52beaf4d3c4df8b
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-22 18:52:15 -0300

    Section aboutMe is done!

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -3,17 +3,22 @@ import { motion } from 'framer-motion'
 
 export function AboutMe() {
   return (
-    <div
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
       className="h-screen flex relative flex-col text-center
      md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl">
-        Sobre mim!
+      <h3 className="absolute top-14 lg:top-24 uppercase tracking-[10px] md:tracking-[20px] text-gray-500 text-xl">
+        Sobre
       </h3>
 
       <motion.img
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full 
-        object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+        className=" mt-52 md:mt-28 lg:mt-0 md:mb-0 flex-shrink-0 w-40 h-40 rounded-full 
+        object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
         src="https://avatars.githubusercontent.com/u/92957086?v=4"
         initial={{
           x: -200,
@@ -32,37 +37,30 @@ export function AboutMe() {
           <span className="underline decoration-[#F7AB04]/50">pequeno </span>
           textinho legal!
         </h4>
-        <p className="text-sm">
+        <p className="text-base max-h-56">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
           adipisci dolores neque, odio qui reprehenderit minus odit aspernatur
           harum iure accusamus magnam reiciendis sint itaque aliquam natus
-          beatae repellendus. Laborum! Eos, blanditiis! Adipisci pariatur
-          ratione corrupti laborum omnis nam dolor iure quasi, eum, sed expedita
-          earum nisi sunt beatae molestiae! In consequatur quam quibusdam
-          suscipit quas dolorum delectus pariatur rerum! A dolor optio, eum
-          maiores fugit nisi, dignissimos mollitia temporibus error dolore
-          nostrum blanditiis, ad corrupti tempore repudiandae adipisci officia
-          aliquam porro quisquam ab! Impedit adipisci animi numquam et ullam?
-          Nesciunt ullam impedit ex eveniet optio laudantium odit, nostrum
-          voluptates saepe voluptatem illum explicabo. Ipsum dolore veniam neque
-          provident facere est iusto omnis tenetur nisi? Tempora provident quos
-          culpa vitae. Sed a asperiores optio deleniti distinctio culpa
-          molestiae odio! Architecto unde esse vitae veniam sapiente, pariatur
-          fugit laboriosam necessitatibus aperiam provident voluptatem ut,
-          excepturi eaque! Vel ab labore accusamus doloribus. Consectetur
-          repellat voluptatibus quis rem provident, ad veniam ipsum pariatur
-          modi culpa porro, ratione consequatur libero. Incidunt temporibus
-          impedit tempora quibusdam aliquam, consequuntur earum quam
-          perspiciatis non tenetur totam. Provident? Error exercitationem aut
-          sint cum, itaque corrupti, esse obcaecati facere quas delectus harum
-          veniam assumenda eius vero neque praesentium ut magni distinctio
-          provident. Eaque iste deserunt cumque eius. Voluptatibus, sed? Libero
-          deleniti ut minus enim, neque itaque, ullam voluptatum eligendi
-          quibusdam quisquam pariatur dolore veritatis ipsam quam similique
-          adipisci veniam vero accusantium dolorum! Maxime ipsam officiis itaque
-          fugiat dicta nisi.
+          beatae repellendus. Laborum! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Maiores adipisci dolores neque, odio qui
+          reprehenderit minus odit aspernatur harum iure accusamus magnam
+          reiciendis sint itaque aliquam natus beatae repellendus. Laborum!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+          adipisci dolores neque, odio qui reprehenderit minus odit aspernatur
+          harum iure accusamus magnam reiciendis sint itaque aliquam natus
+          beatae repellendus. Laborum! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Maiores adipisci dolores neque, odio qui
+          reprehenderit minus odit aspernatur harum iure accusamus magnam
+          reiciendis sint itaque aliquam natus beatae repellendus. Laborum!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+          adipisci dolores neque, odio qui reprehenderit minus odit aspernatur
+          harum iure accusamus magnam reiciendis sint itaque aliquam natus
+          beatae repellendus. Laborum! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Maiores adipisci dolores neque, odio qui
+          reprehenderit minus odit aspernatur harum iure accusamus magnam
+          reiciendis sint itaque aliquam natus beatae repellendus. Laborum!
         </p>
       </div>
-    </div>
+    </motion.div>
   )
 }
